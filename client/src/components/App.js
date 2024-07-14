738a3734a22e4276bee9c2c1358aa96a2c402467
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-07-14 13:13:19 +0300

    add sign up part

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,21 @@
-// src/App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LocationList from './LocationList';
 import LocationDetails from './LocationDetails';
 import Navbar from './Navbar';
+import AuthForm from './AuthForm';
+import PrivateRoute from './PrivateRoute';
 import './App.css'; 
+
 const App = () => {
   return (
     <Router>
       <div className="app-container">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={LocationList} />
-          <Route path="/location/:id" component={LocationDetails} />
+          <Route path="/" exact component={AuthForm} />
+          <PrivateRoute path="/locations" exact component={LocationList} />
+          <PrivateRoute path="/location/:id" component={LocationDetails} />
         </Switch>
       </div>
     </Router>
